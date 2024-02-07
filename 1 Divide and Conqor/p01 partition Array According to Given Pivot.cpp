000f588f13365536e49d4b4781143c1509fd687c
gvsrgh <148122487+gvsrgh@users.noreply.github.com>
2024-02-07 09:52:22 +0530

    Create p01 partition Array According to Given Pivot.cpp

---
+++ 1 Divide and Conqor/p01 partition Array According to Given Pivot.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+    
+        
+        vector<int> final1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<pivot){
+                final1.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==pivot){
+                final1.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>pivot){
+                final1.push_back(nums[i]);
+            }
+        }
+        
+       
+
+        return final1;
+    }
+};
