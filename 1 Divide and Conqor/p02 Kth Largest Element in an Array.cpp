42c313565f1c0d4da9e4177c67e4b4bb60e3156d
gvsrgh <148122487+gvsrgh@users.noreply.github.com>
2024-02-07 09:53:18 +0530

    Create p02 Kth Largest Element in an Array.cpp

---
+++ 1 Divide and Conqor/p02 Kth Largest Element in an Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        sort(nums.begin(), nums.end());
+
+        int i = nums.size() - k;
+
+        return nums[i];
+    }
+};
