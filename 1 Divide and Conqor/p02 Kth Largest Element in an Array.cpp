2b6ef006fa90eb19245fdaf959d197eab5208acf
gvsrgh <148122487+gvsrgh@users.noreply.github.com>
2024-02-07 10:00:19 +0530

    Update p02 Kth Largest Element in an Array.cpp
    https://leetcode.com/problems/kth-largest-element-in-an-array/description/

--- 1 Divide and Conqor/p02 Kth Largest Element in an Array.cpp
+++ 1 Divide and Conqor/p02 Kth Largest Element in an Array.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) 
     {
+        
         sort(nums.begin(), nums.end());
 
         int i = nums.size() - k;
 
         return nums[i];
+        
     }
 };
